744eda347be77a978573eb189f4273da2a025a29
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2020-07-30 12:07:07 -0600

    Adds movies and jsonify

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+movies = [
+    {
+        "name": "The Shawshank Redemption",
+        "cast": ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"],
+        "genres": ["Drama"]
+    },
+    {
+        "name": "The Godfather",
+        "casts": ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
+        "genres": ["Crime", "Drama"]
+    },
+    {
+        "name": "The Hobbit",
+        "casts": ["Martin Freeman", "Richard Armitage", "Ian McKellen", "PeterJackson"],
+        "genres": ["Fantasy", "Drama"]
+    }
+]
 
 
-@app.route('/')
-def hello_world():
-    return 'How are you, Wonderful Ms. Provosty?'
+@app.route('/movies')
+def return_movies():
+    return jsonify(movies)
 
 
 if __name__ == '__main__':
