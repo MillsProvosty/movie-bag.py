6362e2cb7e9a0b56dc0659c64c24e019b00de793
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2020-07-31 13:29:34 -0600

    Adds delete functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -24,6 +24,20 @@ movies = [
 @app.route('/movies')
 def return_movies():
     return jsonify(movies)
+
+
+@app.route('/movies', methods=['POST'])
+def add_movie():
+    movie = request.get_json()
+    movies.append(movie)
+    return {'id': len(movies)}, 200
+
+
+@app.route('/movies/<int:index>', methods=['DELETE'])
+def delete_movie(index):
+    get_movie = request.get_data(index)
+    movies.__delitem__(get_movie)
+    return movies, 200
 
 
 if __name__ == '__main__':
